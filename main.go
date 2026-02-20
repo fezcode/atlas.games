f60cb8ae9f710b8b6bbf86f0c4bf8d1d79065630
fezcode <samil.bulbul@gmail.com>
2026-02-20 23:35:47 +0300

    feat: add version flag support

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 	"atlas.games/internal/game"
 )
 
+var Version = "dev"
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("atlas.games v%s\n", Version)
+		return
+	}
+
 	p := tea.NewProgram(game.NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error running game: %v\n", err)
